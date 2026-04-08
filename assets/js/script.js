1aa9a6912ebdc990ed6958cacf9ee4b17b5919fa
day280130 <dimas.a280130@gmail.com>
2026-04-08 13:13:32 +0700

    refactor: improved code readability part 2 and html semantics

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-// Image URL
-const imgUrl = "./assets/img";
-
-// Applying imgUrl
-const imgElements = document.querySelectorAll("img");
-imgElements.forEach((imgElement) => {
-  imgElement.src = `${imgUrl}/${imgElement.dataset.imgName}`;
-});
-
 // Sidebar
 const button = document.querySelector(".sidebar-toggle");
 const asideElement = document.querySelector("aside");
@@ -18,24 +9,31 @@ button.addEventListener("click", () => {
 
 // Dropdown
 const dropdowns = document.querySelectorAll(".dropdown");
-dropdowns.forEach((e) => {
+dropdowns.forEach((dropdown) => {
   // attach dropdown state handler
-  const dropdownTrigger = e.querySelector(".dropdown-trigger");
+  const dropdownTrigger = dropdown.querySelector(".dropdown-trigger");
   dropdownTrigger.addEventListener("click", () => {
-    const currentState = e.dataset.open;
+    const currentState = dropdown.dataset.open;
     const nextState = currentState === "true" ? false : true;
-    e.setAttribute("data-open", nextState);
+    dropdown.setAttribute("data-open", nextState);
   });
   // remove prevent-animation helper after page loaded
-  const container = e.querySelector(".dropdown-container");
+  const dropdownContainer = dropdown.querySelector(".dropdown-container");
   document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-      container.classList.remove("prevent-animation");
-    }, 300);
+      dropdownContainer.classList.remove("prevent-animation");
+    }, 10);
   });
+  // handle clicking outside dropdown container
   window.addEventListener("click", (mv) => {
-    const openState = e.dataset.open;
-    if (openState === "true" && !e.contains(mv.target)) e.setAttribute("data-open", false);
+    const isDropdownOpen = dropdown.dataset.open === "true";
+    const isDropdownContainer = dropdownContainer.contains(mv.target);
+    // omitting dropdown trigger click here since it will be handled by its own click event listener
+    // and not omitting it result in dropdown close immediately after open when clicking the trigger
+    const isDropdownTrigger = dropdownTrigger.contains(mv.target);
+    if (isDropdownOpen && !isDropdownContainer && !isDropdownTrigger) {
+      dropdown.setAttribute("data-open", false);
+    }
   });
 });
 
@@ -85,120 +83,3 @@ mql.onchange = () => {
     body.setAttribute("data-theme", "light");
   }
 };
-
-// Render Cards
-const renderCards = (card, parent) =>
-  (parent.innerHTML += `
-    <a href="${card.link}" target="_blank" style="--shadow: ${card.shadow}">
-      <img src="${imgUrl}/${card.id}.png" alt="${card.id} icon" />
-      ${card.name}
-    </a>
-  `);
-const languages = [
-  {
-    id: "html",
-    name: "HTML",
-    link: "https://html.com/",
-    shadow: "28 100% 64%",
-  },
-  {
-    id: "css",
-    name: "CSS",
-    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-    shadow: "200 100% 55%",
-  },
-  {
-    id: "js",
-    name: "Javascript",
-    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    shadow: "53 100% 55%",
-  },
-  {
-    id: "ts",
-    name: "Typescript",
-    link: "https://www.typescriptlang.org/",
-    shadow: "204 69% 42%",
-  },
-  {
-    id: "php",
-    name: "PHP",
-    link: "https://www.php.net/docs.php",
-    shadow: "236 13% 55%",
-  },
-  {
-    id: "java",
-    name: "Java",
-    link: "https://docs.oracle.com/en/java/",
-    shadow: "202 59% 35%",
-  },
-];
-const languagesDiv = document.getElementById("languages-container");
-languages.forEach((language) => renderCards(language, languagesDiv));
-const frameworks = [
-  {
-    id: "react",
-    name: "ReactJS",
-    link: "https://react.dev/",
-    shadow: "184 100% 50%",
-  },
-  {
-    id: "express",
-    name: "ExpressJS",
-    link: "https://expressjs.com/",
-    shadow: "360 5% 23%",
-  },
-  {
-    id: "tailwind",
-    name: "TailwindCSS",
-    link: "https://tailwindcss.com/",
-    shadow: "189 100% 45%",
-  },
-  {
-    id: "next",
-    name: "NextJS",
-    link: "https://nextjs.org/",
-    shadow: "0 0% 0%",
-  },
-  {
-    id: "ci",
-    name: "CodeIgniter",
-    link: "https://www.codeigniter.com/",
-    shadow: "14 100% 53%",
-  },
-];
-const frameworksDiv = document.getElementById("frameworks-container");
-frameworks.forEach((framework) => renderCards(framework, frameworksDiv));
-const tools = [
-  {
-    id: "mysql",
-    name: "MySQL",
-    link: "https://www.mysql.com/",
-    shadow: "196 64% 27%",
-  },
-  {
-    id: "mongodb",
-    name: "MongoDB",
-    link: "https://www.mongodb.com/",
-    shadow: "135 75% 50%",
-  },
-  {
-    id: "vite",
-    name: "ViteJS",
-    link: "https://vitejs.dev/",
-    shadow: "280 75% 50%",
-  },
-  {
-    id: "nodejs",
-    name: "NodeJS",
-    link: "https://nodejs.org/en",
-    shadow: "142 86% 29%",
-  },
-  {
-    id: "vscode",
-    name: "Visual Studio Code",
-    link: "https://code.visualstudio.com/",
-    shadow: "208 100% 63%",
-  },
-];
-const toolsDiv = document.getElementById("tools-container");
-tools.forEach((tool) => renderCards(tool, toolsDiv));
